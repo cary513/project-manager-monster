6796b95900ddb4087cf455c5de0bc8413beaec3d
cary513 <qegg513@gmail.com>
2026-02-16 12:30:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-{
-  "bingo_id": "user_001_2026",
-  "title": "2026 數位遊牧挑戰",
-  "cells": [
-    {"id": 1, "content": "法文檢定通過", "status": true},
-    {"id": 2, "content": "Python 腳本跑通", "status": false},
-    ...
-  ],
-  "is_completed": false
-}
 import streamlit as st
 import numpy as np
 
